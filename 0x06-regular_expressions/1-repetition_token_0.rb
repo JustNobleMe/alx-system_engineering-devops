0a980a758558f9a1f62af121eb88596362dc6391
Noble <84409340+JustNobleMe@users.noreply.github.com>
2023-07-06 10:27:30 +0000

    repetition

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# A regular expression that is simply matching hbtth
-puts ARGV[0].scan(/hbt{2,6}n/).join
+# A regular expression that is simply matching repetition of t
+puts ARGV[0].scan(/hbt{2,5}n/).join
